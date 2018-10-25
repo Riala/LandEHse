8dfc95b08a3c6c9aa7c30d449a30aa80e2398815
Nelaiam <maialen.teillery64@gmail.com>
2018-10-25 21:23:23 +0200

    1st Version
    Start project 25.10.2018 21:22

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+    include 'plats.php';
+
+    platscuisines_V01();
---
+++ plats.php
@@ -0,0 +1,25 @@
+<?php
+function platscuisines_V00(){
+    /* Programme servant au calcul de la tendance en fonction des critères utilisateur
+     * Auteurs : Jean AGUIRRE et Maialen TEILLERY
+     * Date : 24/10/2018
+     * Tendance : pas encore calculée
+     */
+    // Déclaration des variables
+    //On calcule le prix d'une facture
+
+}
+
+function platscuisines_V01(){
+    /* Programme servant au calcul de la tendance en fonction des critères utilisateur
+     * Auteurs : Jean AGUIRRE et Maialen TEILLERY
+     * Date : 24/10/2018
+     * Tendance : pas encore calculée
+     */
+
+    // Déclaration des variables
+    $commande[12][4] = {{0,0,1,2}, {}};
+    echo $commande[1];
+    //On calcule le prix d'une facture
+
+}
