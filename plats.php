0b4cb48c282316fe8f73b0db41959c8b90f4c762
Nelaiam <maialen.teillery64@gmail.com>
2018-10-26 09:04:29 +0200

    Update plats.php

--- plats.php
+++ plats.php
@@ -19,12 +19,63 @@ function platscuisines_V01(){
 
     // Déclaration des variables
 
-    $commande = array("Chipirons"=>array(1=>0,
-                                        2=>0,
+    $commande = array("Chipirons"=>array(2=>0,
                                         4=>1,
-                                        8=>2));
+                                        8=>2)),
+                array("Axoa"=>array(1=>25,
+                                    2=> 0,
+                                    3=>25,
+                                    4=>0)),
+                array("GateauB"=>array(4 => 2,
+                                      8 => 4)),
+                array("PotAuFeu"=>array(1 => 3,
+                                      2 => 4,
+                                      4 => 5,
+                                      8 => 10)),
+                array("BoeufBourguignon"=>array(1 => 2,
+                                  2 => 0,
+                                  4 => 10,
+                                  8 => 0)),
+                array("EscargotsAilPersil"=>array(1 => 0,
+                                      2 => 5)),
+                array("AssietteLandaise"=>array(1 => 3,
+                                      2 => 2,
+                                      4 => 10,
+                                      8 => 5)),
+                array("Tourtiere"=>array(4 => 4,
+                                      8 => 2)),
+                array("Pastis"=>array(4 => 5,
+                                      8 => 5)));
+
+    $prix = array("Chipirons"=>array(2=>0,
+                                     4=>1,
+                                     8=>2)),
+                array("Axoa"=>array(1=>25,
+                                    2=> 0,
+                                    3=>25,
+                                    4=>0)),
+                array("GateauB"=>array(4 => 2,
+                                      8 => 4)),
+                array("PotAuFeu"=>array(1 => 3,
+                                      2 => 4,
+                                      4 => 5,
+                                      8 => 10)),
+                array("BoeufBourguignon"=>array(1 => 2,
+                                  2 => 0,
+                                  4 => 10,
+                                  8 => 0)),
+                array("EscargotsAilPersil"=>array(1 => 0,
+                                      2 => 5)),
+                array("AssietteLandaise"=>array(1 => 3,
+                                      2 => 2,
+                                      4 => 10,
+                                      8 => 5)),
+                array("Tourtiere"=>array(4 => 4,
+                                      8 => 2)),
+                array("Pastis"=>array(4 => 5,
+                                      8 => 5)),);
     echo $commande["Chipirons"][4];
-    
+
     //On calcule le prix d'une facture
 
 }
